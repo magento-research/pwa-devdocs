76006571188d437adda340b4398ede0728e4056a
James Calcaben <jcalcaben@magento.com>
2018-04-27 10:28:02 -0500

    Rename accordion into collapsible and refactor
    Collapsible is a more accurate name for the functionality.
    
    TOC has been refactored to fit changes in base theme.

--- src/_js/index.js
+++ src/_js/index.js
@@ -1,13 +1,19 @@
 // Javascript goes here
 
-jquery = require("jquery/dist/jquery.slim");
+jquery = require("jquery/dist/jquery");
 
-// Add menu accordion functionality
-var Accordion = require("./accordion");
+// Add menu collapsible functionality
+var Collapsible = require("./collapsible");
 
 jquery(".toc-entry").each( (index, control) => {
-    Accordion.apply(control,jquery(control).next(".table-of-contents"));
+    Collapsible.apply(control,jquery(control).next(".table-of-contents"));
 });
 
-// Open the active TOC
-jquery(".table-of-contents li.active").parents(".table-of-contents").prev(".toc-entry").click();
+// Open the active TOC with no delay
+let activeTocSection = jquery(".table-of-contents li.active").parents(".menu-section");
+
+Collapsible.toggle(
+    jquery(activeTocSection).find(".toc-entry i"),
+    jquery(activeTocSection).children(".table-of-contents"),
+    0
+);
