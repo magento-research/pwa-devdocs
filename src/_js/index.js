f18d38a6f63ca2e1c0558d7f7eaa564c9e8a9933
James Calcaben <jcalcaben@magento.com>
2018-05-15 15:00:41 -0500

    Resolve style and layout conflicts with new look and feel changes

--- src/_js/index.js
+++ src/_js/index.js
@@ -1,19 +1,1 @@
 // Javascript goes here
-
-jquery = require("jquery/dist/jquery");
-
-// Add menu collapsible functionality
-var Collapsible = require("./collapsible");
-
-jquery(".toc-entry").each( (index, control) => {
-    Collapsible.apply(control,jquery(control).next(".table-of-contents"));
-});
-
-// Open the active TOC with no delay
-let activeTocSection = jquery(".table-of-contents li.active").parents(".menu-section");
-
-Collapsible.toggle(
-    jquery(activeTocSection).find(".toc-entry i"),
-    jquery(activeTocSection).children(".table-of-contents"),
-    0
-);
